12ad47d539455a5ee7e0717e51eb94d2d4cd2699
ANCHAL AITHANI <anchalaithani@gmail.com>
2019-12-31 00:53:02 +0530

    munging left

--- Files_to_be_executed/test.py
+++ Files_to_be_executed/test.py
@@ -16,10 +16,51 @@ hair_salon_df.to_csv(base_address + "\Hair-Salon-Establishment\dataset\hair_salo
 """ assigning 2 to morning 0 to afternoon and 1 to evening """
 # creating labelEncoder for normalising the label
 le = preprocessing.LabelEncoder()
+
 book_tod_encoded = le.fit_transform(hair_salon_df["book_tod"])
-# print(" ENCODED : ", book_tod_encoded)
 hair_salon_df["book_tod_encoded"] = book_tod_encoded
 hair_salon_df.drop("book_tod", axis=1, inplace=True)
+
+book_dow_encoded = le.fit_transform(hair_salon_df["book_dow"])
+hair_salon_df["book_dow_encoded"] = book_dow_encoded
+hair_salon_df.drop("book_dow", axis=1, inplace=True)
+
+
+book_category_encoded = le.fit_transform(hair_salon_df["book_category"])
+hair_salon_df["book_category_encoded"] = book_category_encoded
+hair_salon_df.drop("book_category", axis=1, inplace=True)
+
+
+book_staff_encoded = le.fit_transform(hair_salon_df["book_staff"])
+hair_salon_df["book_staff_encoded"] = book_staff_encoded
+hair_salon_df.drop("book_staff", axis=1, inplace=True)
+
+
+last_category_encoded = le.fit_transform(hair_salon_df["last_category"])
+hair_salon_df["last_category_encoded"] = last_category_encoded
+hair_salon_df.drop("last_category", axis=1, inplace=True)
+
+
+last_staff_encoded = le.fit_transform(hair_salon_df["last_staff"])
+hair_salon_df["last_staff_encoded"] = last_staff_encoded
+hair_salon_df.drop("last_staff", axis=1, inplace=True)
+
+
+last_receipt_tot_encoded = le.fit_transform(hair_salon_df["last_receipt_tot"])
+hair_salon_df["last_receipt_tot_encoded"] = last_receipt_tot_encoded
+hair_salon_df.drop("last_receipt_tot", axis=1, inplace=True)
+
+
+last_dow_encoded = le.fit_transform(hair_salon_df["last_dow"])
+hair_salon_df["last_dow_encoded"] = last_dow_encoded
+hair_salon_df.drop("last_dow", axis=1, inplace=True)
+
+
+last_tod_encoded = le.fit_transform(hair_salon_df["last_tod"])
+hair_salon_df["last_tod_encoded"] = last_tod_encoded
+hair_salon_df.drop("last_tod", axis=1, inplace=True)
+
+
 hair_salon_df.to_csv(base_address + "\Hair-Salon-Establishment\dataset\hair_salon_munged.csv", index=False)
 # print(hair_salon_df["book_tod"])
 
