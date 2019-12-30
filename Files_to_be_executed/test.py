10d9b343f192213a787896f25d2ffff6a46b1de2
ANCHAL AITHANI <anchalaithani@gmail.com>
2019-12-31 00:40:05 +0530

    munging left

--- Files_to_be_executed/test.py
+++ Files_to_be_executed/test.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from sklearn import preprocessing
 base_address = "F:\Pycharm"
 hair_salon_df = pd.read_csv(base_address + "\Hair-Salon-Establishment\dataset\hair_salon_no_show_wrangled_df.csv")
 
@@ -13,15 +13,13 @@ for column in columns_to_be_dropped:
 # hair_salon_df.drop(hair_salon_df.index[[0]], inplace=True)
 hair_salon_df.to_csv(base_address + "\Hair-Salon-Establishment\dataset\hair_salon_munged.csv", index=False)
 
-""" assigning 0 to morning 1 to afternoon and 2 to evening """
-for questions in range(1):
-    for index, row in hair_salon_df.iterrows():
-        print(row[1])
-        if(row[1]) == "morning":
-            row[1] = "0"
-        elif (row[1]) == "afternoon":
-            row[1] = "1"
-        elif(row[1]) == "evening":
-            row[1] = "2"
-print(hair_salon_df["book_tod"])
+""" assigning 2 to morning 0 to afternoon and 1 to evening """
+# creating labelEncoder for normalising the label
+le = preprocessing.LabelEncoder()
+book_tod_encoded = le.fit_transform(hair_salon_df["book_tod"])
+# print(" ENCODED : ", book_tod_encoded)
+hair_salon_df["book_tod_encoded"] = book_tod_encoded
+hair_salon_df.drop("book_tod", axis=1, inplace=True)
+hair_salon_df.to_csv(base_address + "\Hair-Salon-Establishment\dataset\hair_salon_munged.csv", index=False)
+# print(hair_salon_df["book_tod"])
 
